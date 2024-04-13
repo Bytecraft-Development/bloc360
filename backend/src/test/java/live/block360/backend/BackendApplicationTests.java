c07acdc35c043c6279c809c43db18556ea7148f6
Adrian Bucur <adrianbucur83@gmail.com>
2024-04-13 16:13:17 +0300

    everything runs

--- backend/src/test/java/live/block360/backend/BackendApplicationTests.java
+++ backend/src/test/java/live/block360/backend/BackendApplicationTests.java
@@ -1,9 +1,14 @@
 package live.block360.backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 class BackendApplicationTests {
 
 	@Test
