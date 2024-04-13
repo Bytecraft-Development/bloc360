1534820c85444d9f77316942ee11de08d2cdcf84
Adrian Bucur <adrianbucur83@gmail.com>
2024-04-13 15:16:02 +0300

    project backbone

---
+++ backend/src/test/java/live/block360/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package live.block360.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
