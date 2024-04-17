383a2ab334b65c880d7594eb075be02884e7a6ad
LMN <georgelemnosanu@gmail.com>
2024-04-18 00:23:55 +0300

    Revert "Association"
    This reverts commit 56c0bac221968ab5f4f1c3de8a5a8bb65cc0259f.

--- backend/src/main/java/live/block360/backend/Service/ServiceInterface/AssociationService.java
+++
@@ -1,8 +0,0 @@
-package live.block360.backend.Service.ServiceInterface;
-
-import live.block360.backend.dto.createDTO.CreateAssociationDTO;
-import live.block360.backend.model.Association;
-
-public interface AssociationService {
-    Association createAssociation(CreateAssociationDTO  createAssociationDTO);
-}
