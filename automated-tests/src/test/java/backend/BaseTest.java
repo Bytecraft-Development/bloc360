1eb603a6f06439cfd5e8a43fe2275fb728898091
alexrmn <roman.alex.93@gmail.com>
2024-04-20 10:52:58 +0300

    changed to normal gradle file from Kotlin

--- automated-tests/src/test/java/backend/BaseTest.java
+++ automated-tests/src/test/java/backend/BaseTest.java
@@ -1,15 +1,6 @@
 package backend;
 
-
-
-
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-
-import java.io.IOException;
 import java.net.http.HttpClient;
-import java.net.http.HttpResponse;
-
 
 public class BaseTest {
 
