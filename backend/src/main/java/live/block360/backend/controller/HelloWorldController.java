8645d65c323358f180c59fe276864755d517e8cf
Adrian Bucur <adrianbucur83@gmail.com>
2024-04-22 18:21:54 +0300

    working security

--- backend/src/main/java/live/block360/backend/controller/HelloWorldController.java
+++ backend/src/main/java/live/block360/backend/controller/HelloWorldController.java
@@ -1,18 +1,19 @@
 package live.block360.backend.controller;
 
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
-@Profile("security")
 public class HelloWorldController {
-
     @GetMapping("/hello")
-    public String helloWorld() {
-        return "Hello 16 aprilie 2024";
+    public String helloWorld(Authentication authentication) {
+        return "Hello users";
     }
 
 }
