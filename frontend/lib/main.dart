ca1764ad2e7ce1584b8f816af18f91210d12a4e5
GeorgeLMN <georgelemnosanu@gmail.com>
2024-05-16 13:39:25 +0300

    go router and registration page added

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -3,11 +3,12 @@ import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:frontend/pages/privacy_policy.dart';
 import 'package:frontend/pages/tos_page.dart';
 import 'package:frontend/views/main/main_view.dart';
+import 'package:frontend/controller/simple_ui_controller.dart';
+import 'package:go_router/go_router.dart';
 import 'package:get/get.dart';
-
+import 'RegistrationPageTest.dart';
 import 'LoginPageTest.dart';
 import 'ExpensePage.dart';
-import 'controller/simple_ui_controller.dart';
 
 void main() {
   usePathUrlStrategy();
@@ -15,19 +16,43 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final GoRouter _router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => MainView(),
+      ),
+      GoRoute(
+        path: '/login-page',
+        builder: (context, state) => LoginView(),
+      ),
+      GoRoute(
+        path: '/tos',
+        builder: (context, state) => TermsOfService(),
+      ),
+      GoRoute(
+        path: '/privacy',
+        builder: (context, state) => PrivacyPolicy(),
+      ),
+      GoRoute(
+        path: '/expenses',
+        builder: (context, state) => ExpensePage(),
+      ),
+      GoRoute(
+        path: '/register',
+        builder: (context, state) => SignUpView(),
+      ),
+    ],
+  );
+
   @override
   Widget build(BuildContext context) {
     Get.put(SimpleUIController());
-    return MaterialApp(
-      initialRoute: '/login-page',
-      routes: {
-        '/login-page': (context) => LoginView(),
-        '/tos': (context) => TermsOfService(),
-        '/privacy': (context) => const PrivacyPolicy(),
-        '/expenses': (context) =>ExpensePage(),
-      },
+    return MaterialApp.router(
+      routerDelegate: _router.routerDelegate,
+      routeInformationParser: _router.routeInformationParser,
+      routeInformationProvider: _router.routeInformationProvider,
       title: 'Bloc360',
-      home: MainView(),
     );
   }
 }
