5831c9c16170e657b542746e3e62b8de287d0d4f
GeorgeLMN <georgelemnosanu@gmail.com>
2024-05-19 19:00:57 +0300

    test redirect

--- frontend/lib/pages/association.dart
+++ frontend/lib/pages/association.dart
@@ -1,34 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/config/environment.dart';
 import 'package:go_router/go_router.dart';
 import 'package:http/http.dart' as http;
 import 'dart:html' as html;
 
 
+class AssociationView extends StatefulWidget {
+  @override
+  _AssociationViewState createState() => _AssociationViewState();
+}
 
-Future<void> checkRoleAndRedirect(BuildContext context) async {
-  String? accessToken = html.window.localStorage['access_token'];
-  const API_URL = String.fromEnvironment('API_URL');
-  final String baseUrl = API_URL;
-
-  if (accessToken == null) {
-    throw Exception('No access token found, please log in.');
+class _AssociationViewState extends State<AssociationView> {
+  @override
+  void initState() {
+    super.initState();
+    _checkRoleAndRedirect();
   }
-  final response = await http.get(
-    Uri.parse('$baseUrl/redirectByRole'),
-    headers: {
-      'Authorization': 'Bearer $accessToken',
-    },
-  );
 
-  if (response.statusCode == 200) {
-    final redirectUrl = response.body;
-    if (redirectUrl.contains("admin/dashboard")) {
-      context.go('/hello_world');
-    } else {
-      context.go('/cui_input');
+  Future<void> _checkRoleAndRedirect() async {
+    String? accessToken = html.window.localStorage['access_token'];
+    const API_URL = String.fromEnvironment('API_URL');
+    final String baseUrl = API_URL;
+
+    if (accessToken == null) {
+      throw Exception('No access token found, please log in.');
     }
-  } else {
-    throw Exception('Failed to redirect based on role');
+
+    final response = await http.get(
+      Uri.parse('$baseUrl/redirectByRole'),
+      headers: {
+        'Authorization': 'Bearer $accessToken',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final redirectUrl = response.body;
+      if (redirectUrl.contains("admin/dashboard")) {
+        context.go('/hello_world');
+      } else {
+        context.go('/cui_input');
+      }
+    } else {
+      throw Exception('Failed to redirect based on role');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Association View'),
+      ),
+      body: Center(
+        child: CircularProgressIndicator(), // Afișează un indicator de încărcare până la redirecționare
+      ),
+    );
   }
 }
