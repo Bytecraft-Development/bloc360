00b32cfdd21c20996d2b45ab2b7cc11ffd6bf65f
GeorgeLMN <georgelemnosanu@gmail.com>
2024-08-02 18:34:54 +0300

    small update for refactoring still in progress

--- backend/src/main/java/live/bloc360/backend/controller/UserDashboardController.java
+++ backend/src/main/java/live/bloc360/backend/controller/UserDashboardController.java
@@ -1,7 +1,6 @@
 package live.bloc360.backend.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
-import live.bloc360.backend.service.UserDashboardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
