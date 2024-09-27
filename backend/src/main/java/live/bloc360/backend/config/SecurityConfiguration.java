f7a6ada2187ae9f7dea65ba15605dd78ad4e0e7f
Adrian Bucur <adrianbucur83@gmail.com>
2024-09-27 16:16:05 +0300

    rewrite for SCP #6

--- backend/src/main/java/live/bloc360/backend/config/SecurityConfiguration.java
+++ backend/src/main/java/live/bloc360/backend/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package live.bloc360.backend.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -23,6 +24,7 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@Profile("!test")
 public class SecurityConfiguration {
 
     @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
