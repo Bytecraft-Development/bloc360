10796f1fbc05b4a479f612145dbdaefdab002677
Adrian Bucur <adrianbucur83@gmail.com>
2024-11-30 12:01:00 +0200

    run 9

--- web/src/pages/features/auth/login.js
+++ web/src/pages/features/auth/login.js
@@ -42,7 +42,7 @@ const LoginView = () => {
   const handleLogin = async () => {
     const tokenUrl = process.env.REACT_APP_API_URL;
       const clientId = process.env.REACT_APP_KEYCLOAK_CLIENT_ID;
-      console.log('we made it! YEY! 8');
+      console.log('we made it! YEY! 9');
     try {
       const response = await axios.post(
         tokenUrl, 
