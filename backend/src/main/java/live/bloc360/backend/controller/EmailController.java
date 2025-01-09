fb3b74634c1aa13688fd9910bc4840e9809b618e
Andrea Hegyesi <andrea.hegyesi1@gmail.com>
2025-01-09 19:30:54 +0200

    implemented sendmail with attachment

--- backend/src/main/java/live/bloc360/backend/controller/EmailController.java
+++ backend/src/main/java/live/bloc360/backend/controller/EmailController.java
@@ -1,24 +1,36 @@
-//package live.bloc360.backend.controller;
-//
-//import com.sendgrid.helpers.mail.objects.Content;
-//import live.bloc360.backend.service.EmailServiceSendGrid;
-//import lombok.SneakyThrows;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class EmailController {
-//    private final EmailServiceSendGrid emailServiceSendGrid;
-//
-//    public EmailController(EmailServiceSendGrid emailServiceSendGrid) {
-//        this.emailServiceSendGrid = emailServiceSendGrid;
-//    }
-//
-//
-//    @SneakyThrows
-//    @PostMapping("/sendmail")
-//    public void sendTestEmail() {
-//        emailServiceSendGrid.sendMail("Test email from SpringBoot", new Content("text/plain", "and easy to do anywhere, even with Java"));
-//    }
-//
-//}
+package live.bloc360.backend.controller;
+
+import jakarta.mail.MessagingException;
+import live.bloc360.backend.service.EmailServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class EmailController {
+    private final EmailServiceImpl emailServiceImpl;
+
+    public EmailController(EmailServiceImpl emailServiceImpl) {
+        this.emailServiceImpl = emailServiceImpl;
+    }
+
+    @PostMapping("/sendmail")
+    public ResponseEntity<String> sendEmail(@RequestBody String mail) {
+        try {
+            emailServiceImpl.sendMimeMessage(mail, "Test email from SpringBoot", "This is a test mail");
+            return ResponseEntity.ok("Email sent successfully!");
+        } catch (MessagingException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to send email: MessagingException occurred");
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to send email: IOException occurred");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to send email: An unexpected error occurred");
+        }
+    }
+
+}
+
